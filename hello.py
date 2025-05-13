1a86c5667b71fdf3239b35ac86f8a0eb4cf36956
Alex4386 <psh010209@gmail.com>
2025-05-13 15:00:38 +0900

    feat: update to v2

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import time
 
 while True:
-    print("Hello, Kubernetes! The time is now: {}".format(time.ctime()))
+    print("Hello, Kubernetes V2! The time is now: {}".format(time.ctime()))
     time.sleep(1000)
 
---
+++ update-to-v2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./kubectl set image deployment/hello-deployment python-container=alex4386/hello-pod:v2
