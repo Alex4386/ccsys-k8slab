f94f0d4ba075d7ced3b6aa6b5d47ea541c4c27e5
Sanghee Park <psh010209@gmail.com>
2025-05-15 19:45:06 +0900

    chore: update to v2

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import time
 
 while True:
-    print("Hello, Sanghui Park V1! The time is now: {}".format(time.ctime()))
+    print("Hello, Sanghui Park V2! The time is now: {}".format(time.ctime()))
     time.sleep(1000)
 
